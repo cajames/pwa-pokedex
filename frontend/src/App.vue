a3c853cb0d09715ff8ce4a738601d08be41d4e22
Chris James <chris@webglowit.net>
2018-02-28 02:12:13 +1100

    Inititalised frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1 class="text-red">Working</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
